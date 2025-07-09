8978309873878e4738c53509d2c764c8c5ec3ae7
Sunny <75664409+Sunny-esc@users.noreply.github.com>
2025-07-09 11:31:08 +0530

    Update user.model.js

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -9,7 +9,9 @@ const userSchema = new mongoose.Schema({
   
   password: {
     type: String,
-    required: [true, "Your password is required"],
+    required: function () {
+      return !this.googleId;
+    }
   },
   username: {
     type: String,
